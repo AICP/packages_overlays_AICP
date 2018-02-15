ec01220b1e717711fa1fd56004f89a22250c5ed8
SpiritCroc <spiritcroc@gmail.com>
2018-02-15 21:03:30 +0100

    Initial scripts for Default Dark import

---
+++ generate_default_dark.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+my_path="$(dirname "$0")"
+
+# Get overlay generation functions
+. "$my_path"/generate_overlay.sh
+
+dd_path="../../../external/DefaultDarkTheme-oms"
+
+if [ ! -d "$dd_path" ]; then
+    echo "Cannon access $dd_path"
+    exit 1
+fi
+
+
+# Prepare theme variants
+
+"$dd_path/generate_type3/gen.sh" > /dev/null
+
+
+# Generate themes
+
+overlay_path="$dd_path/app/src/main/assets/overlays"
+overlay_package="com.aicp.overlay.defaultdark"
+generate_overlay "$overlay_path" "$my_path/DefaultDark-System" "android" "$overlay_package" || exit $?
+generate_overlay "$overlay_path" "$my_path/DefaultDark-SystemUI" "com.android.systemui" "$overlay_package" 2:android_8_darker || exit $?
+generate_overlay "$overlay_path" "$my_path/DefaultDark-Settings" "com.android.settings" "$overlay_package" || exit $?
+generate_overlay "$overlay_path" "$my_path/DefaultDark-Calculator" "com.android.calculator2" "$overlay_package" || exit $?
+
+
+# Don't use holo_blue_accent
+rm "$my_path/DefaultDark-System/res/values/type1c.xml"
+find "$my_path/DefaultDark-System" -name "*.xml" -exec sed -i 's|@android:color/holo_blue_bright|@*android:color/accent_material_dark|g' '{}' \;
+find "$my_path/DefaultDark-Settings" -name "*.xml" -exec sed -i 's|@android:color/holo_blue_bright|@*android:color/accent_material_dark|g' '{}' \;
+
+# Remove unimportant styles that break compile
+remove_tag "$my_path/DefaultDark-SystemUI/res/values/dup_gen_styles.xml" "style" "systemui_theme"
+remove_tag "$my_path/DefaultDark-Settings/res/values/styles.xml" "style" "Theme.SubSettingsDialogWhenLarge"
+remove_tag "$my_path/DefaultDark-Settings/res/values/styles.xml" "style" "ThemeOverlay.SwitchBar.Settings"
+remove_tag "$my_path/DefaultDark-Settings/res/values/styles.xml" "style" "ThemeOverlay.SwitchBar.SubSettings"
+remove_tag "$my_path/DefaultDark-Calculator/res/values/type1a.xml" "style" "PadButtonStyle"
+
+# Don't overlay accent, we have extra overlays for that
+remove_tag "$my_path/DefaultDark-System/res/values/colors.xml" "color" "accent_material_light"
+remove_tag "$my_path/DefaultDark-System/res/values/colors.xml" "color" "accent_material_dark"
+remove_tag "$my_path/DefaultDark-System/res/values/colors.xml" "color" "accent_device_default_light"
+remove_tag "$my_path/DefaultDark-System/res/values/colors.xml" "color" "accent_device_default_dark"
+remove_tag "$my_path/DefaultDark-System/res/values/colors.xml" "color" "accent_device_default_700"
+remove_tag "$my_path/DefaultDark-System/res/values/colors.xml" "color" "accent_device_default_50"
+
+# Let's be less aggressive making everything dark to avoid readability issues
+remove_tag "$my_path/DefaultDark-System/res/values/styles.xml" "style" "Theme.DeviceDefault.*?Light.?"
+remove_tag "$my_path/DefaultDark-System/res/values/styles.xml" "style" "Widget.DeviceDefault.*?Light.?"
+remove_tag "$my_path/DefaultDark-SystemUI/res/values/colors.xml" "color" "notification_guts_.*?"
+rm "$my_path/DefaultDark-SystemUI/res/color/notification_guts"*
+
+# Let's not theme PackageInstaller - people can install full theme if they want it
+rm "$my_path/DefaultDark-System/res/drawable"**/perm*
+
+# Clean up other unneeded resources
+rm "$my_path/DefaultDark-System/res/drawable"**/tab_*
+rm "$my_path/DefaultDark-Settings/res/drawable"**/ic_*
+rm "$my_path/DefaultDark-Settings/res/drawable/aicp_logo_bg.png"
+rm "$my_path/DefaultDark-Settings/res/drawable"**/nfc_payment_empty_state.png
+
+# Clean up theme variants
+"$dd_path/generate_type3/clean.sh" > /dev/null
---
+++ generate_overlay.sh
@@ -0,0 +1,171 @@
+my_path="$(dirname "$0")"
+
+function remove_tag() {
+    # Usage:
+    # remove_tag <file> <tag> <name>
+    file="$1"
+    tag_name="$2"
+    name="$3"
+    cat "$file" | tr '\n' '\r' | perl -pe "s|<$tag_name name=\"$name\".*?</$tag_name>||g" | tr '\r' '\n' > "$file.tmp"
+    mv "$file.tmp" "$file"
+}
+
+function generate_overlay() {
+    # Usage:
+    # generate_overlay <source_dir> <target_dir> <target_package> <overlay_package> [variants...]
+    # variants can be
+    #   1:a:<name>
+    #   1:b:<name>
+    #   1:c:<name>
+    #   2:<name>    NOTE: overwriting style/color resources in values* only works when defined in the same file (name-wise)
+    #   3:<name>
+    # NOTE:
+    # Referencing private resources not supported, needs manual intervention
+
+    source_dir="$1"
+    if [ ! -d "$source_dir" ]; then
+        echo "Invalid source overlays dir!"
+        exit 1
+    fi
+
+    target_dir="$2"
+    if [ -z "$target_dir" ]; then
+        echo "Invalid target dir!"
+        exit 1
+    fi
+    name="$(basename "$target_dir")"
+    if [ -z "$name" ]; then
+        echo "Invalid name!"
+        exit 1
+    fi
+
+    target_package="$3"
+    if [ -z "$target_package" ]; then
+        echo "Missing target package!"
+        exit 1
+    fi
+
+    overlay_package_name="$4"
+    if [ -z "$overlay_package_name" ]; then
+        echo "Missing overlay package name!"
+        exit 1
+    fi
+
+    source_overlay="$source_dir/$target_package"
+    if [ ! -d "$source_overlay" ]; then
+        echo "No overlay found for package $target_package!"
+        exit 1
+    fi
+
+    variants="${@:5}"
+
+
+    # --- Target direcory ---
+    rm -rf "$target_dir"
+    mkdir "$target_dir"
+
+
+    # --- Makefile ---
+    makefile="$target_dir/Android.mk"
+
+    echo 'LOCAL_PATH:= $(call my-dir)' > $makefile
+    echo 'include $(CLEAR_VARS)' >> $makefile
+    echo '' >> $makefile
+    echo "LOCAL_RRO_THEME := $name" >> $makefile
+    echo 'LOCAL_CERTIFICATE := platform' >> $makefile
+    echo 'LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res' >> $makefile
+    echo "LOCAL_PACKAGE_NAME := $name" >> $makefile
+    echo '' >> $makefile
+    echo 'include $(BUILD_RRO_SYSTEM_PACKAGE)' >> $makefile
+
+
+    # --- Manifest ---
+
+    manifest="$target_dir/AndroidManifest.xml"
+
+    echo '<manifest xmlns:android="http://schemas.android.com/apk/res/android"' > $manifest
+    echo "package=\"$overlay_package_name.$target_package\"" >> $manifest
+    echo '    android:versionCode="1"' >> $manifest
+    echo '    android:versionName="1.0">' >> $manifest
+    echo >> $manifest
+    echo '    <overlay' >> $manifest
+    echo "        android:targetPackage=\"$target_package\"" >> $manifest
+    echo '        android:priority="1" />' >> $manifest
+    echo >> $manifest
+    echo '</manifest>' >> $manifest
+
+
+    # --- Resources ---
+
+    res_dir="$target_dir/res"
+    source_res_dir="$source_overlay/res"
+    # Type3 variant
+    for variant in $variants; do
+        if [ "${variant:0:2}" = "3:" ]; then
+            source_res_dir="$source_overlay/type3_${variant:2}"
+            if [ ! -d "$source_res_dir" ]; then
+                echo "Type 3 variant not found at $source_res_dir"
+                exit 1
+            fi
+            break
+        fi
+    done
+    cp -r "$source_res_dir" "$res_dir"
+
+
+    # --- Variants type 1+2 ---
+    for variant in $variants; do
+        if [ "${variant:0:2}" = "1:" ]; then
+            # Type 1 variant
+            variant_type="type1${variant:2:1}"
+            target_file="$source_overlay/$variant_type"_"${variant:4}.xml"
+            if [ ! -f "$target_file" ]; then
+                echo "$target_file: target file not found"
+                exit 1
+            fi
+            original_file="$(find "$res_dir" -name "$variant_type.xml")"
+            if [ ! -f "$original_file" ]; then
+                echo "$original_file: original file not found"
+                exit 1
+            fi
+            cp -f "$target_file" "$original_file"
+        elif [ "${variant:0:2}" = "2:" ]; then
+            # Type 2 variant
+            target_dir="$source_overlay/type2_${variant:2}"
+            if [ ! -d "$target_dir" ]; then
+                echo "$target_dir: target dir not found"
+                exit 1
+            fi
+            for d1 in "$target_dir"/*; do
+                if [ -d "$d1" ]; then
+                    f1="$(basename "$d1")"
+                    mkdir -p "$res_dir/$f1"
+                    for d2 in "$d1"/*; do
+                        f2="$(basename "$d2")"
+                        if [ -f "$d2" ]; then
+                            target_file="$res_dir/$f1/$f2"
+                            if [ -f "$target_file" ] && [ "${f1:0:7}" = "values" ]; then
+                                grep "<color" "$d2" | while read -r line; do
+                                    name="$(echo "$line" | sed 's|.*name=\"||' | sed 's|\".*||')"
+                                    remove_tag "$target_file" "color" "$name"
+                                done
+                                grep "<style" "$d2" | while read -r line; do
+                                    name="$(echo "$line" | sed 's|.*name=\"||' | sed 's|\".*||')"
+                                    remove_tag "$target_file" "style" "$name"
+                                done
+                                cp "$d2" "$res_dir/$f1/dup_gen_$f2"
+                            else
+                                cp -f "$d2" "$target_file"
+                            fi
+                        fi
+                    done
+                fi
+            done
+        fi
+    done
+}
+
+if [ ! -z "$1" ]; then
+    # Direct script usage
+    generate_overlay $@
+fi

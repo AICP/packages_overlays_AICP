f8fe0d85b4f28683d12a5bc097012fbf78fbea47
Ali B <abittin@gmail.com>
2018-05-12 22:50:17 +0300

    overlays: do not include lineage and google dark packages
    when aicp dark package is being shipped
    
    Change-Id: Icc41d4d6034b649d2337c4d0c84ae897fbae2347

--- DefaultDark-SystemUI/Android.mk
+++ DefaultDark-SystemUI/Android.mk
@@ -5,5 +5,7 @@ LOCAL_RRO_THEME := DefaultDark-SystemUI
 LOCAL_CERTIFICATE := platform
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_PACKAGE_NAME := DefaultDark-SystemUI
+LOCAL_OVERRIDES_PACKAGES := SysuiDarkThemeOverlay \
+                            LineageDarkTheme
 
 include $(BUILD_RRO_SYSTEM_PACKAGE)

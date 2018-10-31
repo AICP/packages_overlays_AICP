58971572befb487c341adfc4f3a2a2cf3bd3ca50
SpiritCroc <dev@spiritcroc.de>
2018-10-31 13:49:25 +0100

    Clean up for Pie
    Change-Id: I9b3f6f1c7cb350252a9ace1c75f3e3aa34c96614

--- DefaultBlack-System/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_RRO_THEME := DefaultBlack-System
-LOCAL_CERTIFICATE := platform
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_PACKAGE_NAME := DefaultBlack-System
-
-include $(BUILD_RRO_SYSTEM_PACKAGE)

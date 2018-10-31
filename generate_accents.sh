86f9dea6f2fec6aedcdda72ccf8c2b59163cbc65
SpiritCroc <dev@spiritcroc.de>
2018-10-31 15:27:56 +0100

    Add script to generate accents
    Thanks to Moshe for gathering accents :)
    
    Change-Id: Id388112ec865b7589606a0142ed674f0c036ca43

---
+++ generate_accents.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+my_path="$(dirname "$0")"
+
+# Get overlay generation functions
+. "$my_path"/generate_overlay.sh
+
+template="$my_path/accent_template"
+overlay_package="com.aicp.overlay.accent"
+product_packages_makefile="$my_path/product_packages_accent.mk"
+
+function generate_accent() {
+    # Usage:
+    # generate_accent <name> <color_for_light_themes> <color_for_dark_themes> <color_700> <color_50>
+    name="$1"
+    color_lt="$2"
+    color_dk="$3"
+    color_700="$4"
+    color_50="$5"
+    out_dir="$my_path/Accent-$name"
+    name_lc=`echo "$name" | tr '[:upper:]' '[:lower:]'`
+    generate_overlay "$template" "$out_dir" "android" "$overlay_package.$name_lc" "$product_packages_makefile"
+    color_file="$out_dir/res/values/colors.xml"
+    sed -i "s|?color_lt|$color_lt|g" "$color_file"
+    sed -i "s|?color_dk|$color_dk|g" "$color_file"
+    sed -i "s|?color_700|$color_700|g" "$color_file"
+    sed -i "s|?color_50|$color_50|g" "$color_file"
+}
+
+# Clean previous makefile
+rm -f "$product_packages_makefile"
+
+generate_accent "Amber" "#ffa000" "#ffab00" "#ffa000" "#fff8e1"
+generate_accent "BlueLight" "#03a9f4" "#03a9f4" "#0288d1" "#e1f5fe"
+generate_accent "Cyan" "#00bcd4" "#00bcd4" "#0097a7" "#e0f7fa"
+generate_accent "Denim" "#1560BD" "#1560BD" "#1560BD" "#1560BD"
+generate_accent "Gold" "#CFB53B" "#CFB53B" "#CFB53B" "#CFB53B"
+generate_accent "GreenLight" "#8bc34a" "#8bc34a" "#689f38" "#f1f8e9"
+generate_accent "Lime" "#cddc39" "#cddc39" "#afb42b" "#f9fbe7"
+generate_accent "Orange" "#ff9800" "#ff9800" "#f57c00" "#f57c00"
+generate_accent "Oxygen" "#53ADEF" "#53ADEF" "#53ADEF" "#53ADEF"
+generate_accent "Pink" "#e91e63" "#f48fb1" "#c2185b" "#fce4ec"
+generate_accent "Pixel" "#4285f4" "#3367d6" "#fbc02d" "#136ac3"
+generate_accent "Purple" "#673ab7" "#673ab7" "#512da8" "#ede7f6"
+generate_accent "Red" "#ff0000" "#ff0000" "#d32f2f" "#ffebee"
+generate_accent "Teal" "#00bcd4" "#00bcd4" "#0097a7" "#e0f7fa"
+generate_accent "Turquoise" "#00FFFF" "#00FFFF" "#00FFFF" "#00FFFF"
+generate_accent "Yellow" "#fdd835" "#ffeb3b" "#fbc02d" "#fffde7"
---
+++ product_packages.mk
@@ -0,0 +1,2 @@
+include packages/overlays/AICP/product_packages_dark.mk
+include packages/overlays/AICP/product_packages_accent.mk

ea7102a5d72ee6deb3bfcc9a1f39d66f76a580a5
SpiritCroc <dev@spiritcroc.de>
2018-10-31 16:10:38 +0100

    Automatic theme update
    Change-Id: Ia511ba3072ffd22847ea07099a0abe64f532ae2c

---
+++ product_packages_accent.mk
@@ -0,0 +1,17 @@
+PRODUCT_PACKAGES += \
+    Accent-Amber \
+    Accent-GreenLight \
+    Accent-Lime \
+    Accent-Pink \
+    Accent-BlueLight \
+    Accent-Cyan \
+    Accent-Orange \
+    Accent-Purple \
+    Accent-Red \
+    Accent-Teal \
+    Accent-Yellow \
+    Accent-Pixel \
+    Accent-Gold \
+    Accent-Denim \
+    Accent-Oxygen \
+    Accent-Turquoise \

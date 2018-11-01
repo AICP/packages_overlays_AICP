a0d72100c228771b2534631098e9704160321fac
SpiritCroc <dev@spiritcroc.de>
2018-11-01 10:31:51 +0100

    Automatic theme update
    Change-Id: Ia511ba3072ffd22847ea07099a0abe64f532ae2c

---
+++ DefaultDark-Settings/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := DefaultDark-Settings
+LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := DefaultDark-Settings
+
+include $(BUILD_RRO_SYSTEM_PACKAGE)

5f0668d38985081bed5e2fc4e170bbe80ffe51b2
SpiritCroc <dev@spiritcroc.de>
2018-11-01 20:12:44 +0100

    Automatic theme update
    Change-Id: Ia511ba3072ffd22847ea07099a0abe64f532ae2c

---
+++ product_packages_accent.mk
@@ -0,0 +1,17 @@
+PRODUCT_PACKAGES += \
+    Accent-Amber \
+    Accent-BlueLight \
+    Accent-Cyan \
+    Accent-Denim \
+    Accent-Gold \
+    Accent-GreenLight \
+    Accent-Lime \
+    Accent-Orange \
+    Accent-Oxygen \
+    Accent-Pink \
+    Accent-Pixel \
+    Accent-Purple \
+    Accent-Red \
+    Accent-Teal \
+    Accent-Turquoise \
+    Accent-Yellow \
---
+++ product_packages_dark.mk
@@ -0,0 +1,16 @@
+PRODUCT_PACKAGES += \
+    DefaultDark-System \
+    DefaultDark-SystemUI \
+    DefaultDark-Calculator \
+    DefaultDark-Contacts \
+    DefaultDark-DeskClock \
+    DefaultDark-Dialer \
+    DefaultDark-DocumentsUI \
+    DefaultDark-Messaging \
+    DefaultDark-PackageInstaller \
+    DefaultDark-Phone \
+    DefaultDark-Telecom \
+    DefaultDark-SettingsIntelligence \
+    DefaultDark-Settings \
+    DefaultBlack-System \
+    DefaultBlack-SystemUI \

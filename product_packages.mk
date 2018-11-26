70c35d7127ddd674334ea258562809f67c482e34
SpiritCroc <dev@spiritcroc.de>
2018-11-26 17:20:14 +0100

    NonRound-SystemUI for non-round brightness dialog [1/2]
    Change-Id: I766a5fc1c42ae68f589d8c058665a553d3894062

--- product_packages.mk
+++ product_packages.mk
@@ -2,6 +2,7 @@ PRODUCT_PACKAGES += \
     Round-System \
     Round-Launcher \
     NonRound-System \
+    NonRound-SystemUI \
     NonRound-Launcher \
 
 include packages/overlays/AICP/product_packages_dark.mk

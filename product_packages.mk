d8d4f52e5a78c4ad05f8c785d0784e22c6b0a07a
SpiritCroc <dev@spiritcroc.de>
2018-11-28 20:18:28 +0100

    QS shape theming [2/4]
    Change-Id: I9b82121c6e978fed4815240f6baa971f1e21f66b

--- product_packages.mk
+++ product_packages.mk
@@ -4,6 +4,11 @@ PRODUCT_PACKAGES += \
     NonRound-System \
     NonRound-SystemUI \
     NonRound-Launcher \
+    QsShape-Plain-SystemUI \
+    QsShape-PlainAccent-SystemUI \
+    QsShape-Squircle-SystemUI \
+    QsShape-Square-SystemUI \
+    QsShape-RoundSquare-SystemUI \
 
 include packages/overlays/AICP/product_packages_dark.mk
 include packages/overlays/AICP/product_packages_accent.mk

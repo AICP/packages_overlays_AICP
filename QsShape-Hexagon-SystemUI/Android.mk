66adf86c8dcd48778a036389ed1cb0bb504559a3
SpiritCroc <dev@spiritcroc.de>
2018-11-28 20:18:28 +0100

    QS shape theming [2/4]
    Change-Id: I9b82121c6e978fed4815240f6baa971f1e21f66b

---
+++ QsShape-Hexagon-SystemUI/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := QsShape-Hexagon-SystemUI
+LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := QsShape-Hexagon-SystemUI
+LOCAL_RESOURCE_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/APPS/SystemUI_intermediates/package-res.apk
+LOCAL_AAPT_FLAGS := -I $(TARGET_OUT_INTERMEDIATES)/APPS/SystemUI_intermediates/package-res.apk
+
+include $(BUILD_RRO_SYSTEM_PACKAGE)

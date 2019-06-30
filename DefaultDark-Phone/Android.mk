769df4df6c639e4ae53e93982874850372279f35
mosimchah <mosimchah@gmail.com>
2019-06-30 03:05:59 -0400

    Automatic theme update
    Change-Id: I82b623d8a637579b71a6e895988ff035fd61fd51

--- DefaultDark-Phone/Android.mk
+++ DefaultDark-Phone/Android.mk
@@ -6,7 +6,7 @@ LOCAL_CERTIFICATE := platform
 LOCAL_SDK_VERSION := current
 LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 LOCAL_PACKAGE_NAME := DefaultDark-Phone
-LOCAL_RESOURCE_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/APPS/com.android.phone.common_intermediates/package-res.apk
-LOCAL_AAPT_FLAGS := -I $(TARGET_OUT_INTERMEDIATES)/APPS/com.android.phone.common_intermediates/package-res.apk
+LOCAL_RESOURCE_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/APPS/TeleService_intermediates/package-res.apk
+LOCAL_AAPT_FLAGS := -I $(TARGET_OUT_INTERMEDIATES)/APPS/TeleService_intermediates/package-res.apk
 
 include $(BUILD_RRO_SYSTEM_PACKAGE)

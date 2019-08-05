d0ed85af814b05fb012b20fe960a7590a586bec2
SpiritCroc <dev@spiritcroc.de>
2019-08-05 13:15:41 +0200

    Add system icon style setting [4/4]
    Change-Id: Ia81d74126b9630868569e688bb1f8ff18ac1264b

---
+++ OldIcons-Settings/Android.mk
@@ -0,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := OldIcons-Settings
+LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := OldIcons-Settings
+LOCAL_RESOURCE_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/APPS/Settings_intermediates/package-res.apk
+LOCAL_AAPT_FLAGS := -I $(TARGET_OUT_INTERMEDIATES)/APPS/Settings_intermediates/package-res.apk
+
+include $(BUILD_RRO_SYSTEM_PACKAGE)
--- product_packages.mk
+++ product_packages.mk
@@ -10,6 +10,7 @@ PRODUCT_PACKAGES += \
     QsShape-Square-SystemUI \
     QsShape-RoundedSquare-SystemUI \
     QsShape-Hexagon-SystemUI \
+    OldIcons-SystemUI \
 
 include packages/overlays/AICP/product_packages_dark.mk
 include packages/overlays/AICP/product_packages_accent.mk

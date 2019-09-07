bee75ab2c570ad475ea6c93896fd5c69a7ce3254
SpiritCroc <dev@spiritcroc.de>
2019-09-07 11:35:41 +0200

    Remove some overlays for Android 10 bringup
    Change-Id: If4cfd5f2faeeffec3291e8ed79f96b5b114c9ef4

--- product_packages.mk
+++ product_packages.mk
@@ -1,16 +1,1 @@
-PRODUCT_PACKAGES += \
-    Round-System \
-    Round-Launcher \
-    NonRound-System \
-    NonRound-SystemUI \
-    NonRound-Launcher \
-    QsShape-Plain-SystemUI \
-    QsShape-PlainAccent-SystemUI \
-    QsShape-Squircle-SystemUI \
-    QsShape-Square-SystemUI \
-    QsShape-RoundedSquare-SystemUI \
-    QsShape-Hexagon-SystemUI \
-    OldIcons-SystemUI \
-
-include packages/overlays/AICP/product_packages_dark.mk
 include packages/overlays/AICP/product_packages_accent.mk

60c8c0306ec9a3b4f6bd738a932ece98ca593216
SpiritCroc <dev@spiritcroc.de>
2019-09-25 22:30:47 +0200

    Initial static (dark) theming
    Dark theme in Q is managed using the -night resource qualifier.
    So if we want to have common overlays to add dark themes to AOSP apps,
    we can do this most easily using static overlays here.
    
    Let's begin with making the toast dark, other AOSP apps can be imported
    from DarkCroc later.
    
    Change-Id: Ic4e9b87c7ff75e116a6408cb5046a961ee4e067f

--- product_packages.mk
+++ product_packages.mk
@@ -1,3 +1,5 @@
 include packages/overlays/AICP/product_packages_accent.mk
 include packages/overlays/AICP/product_packages_icon_shapes.mk
 include packages/overlays/AICP/product_packages_background_themes.mk
+
+PRODUCT_PACKAGE_OVERLAYS += packages/overlays/AICP/static-overlay

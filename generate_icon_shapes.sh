6ee0f5ce1d71809c7006a36270a897386620dffb
SpiritCroc <dev@spiritcroc.de>
2019-10-01 15:30:28 +0200

    Generate round icon shape
    Wonder why AOSP missed that
    
    Change-Id: Ie1dacfc0f6755e20b26d484f2509ab3ce182b8e0

--- generate_icon_shapes.sh
+++ generate_icon_shapes.sh
@@ -11,11 +11,12 @@ product_packages_makefile="$my_path/product_packages_icon_shapes.mk"
 
 function generate_icon_shape() {
     # Usage:
-    # generate_accent <name> <shape> <dialogCornerRadius> <bottomDialogCornerRadius>
+    # generate_accent <name> <shape> <dialogCornerRadius> <bottomDialogCornerRadius> <useRoundIcon>
     name="$1"
     shape="$2"
     dialogCornerRadius="$3"
     bottomDialogCornerRadius="$4"
+    useRoundIcon="$5"
     out_dir="$my_path/IconShape-$name"
     name_lc=`echo "$name" | tr '[:upper:]' '[:lower:]'`
     generate_overlay "theming_icon_shape_$name_lc" "android.theme.customization.adaptive_icon_shape" "$template" "$out_dir" "android" "$overlay_package.$name_lc" "" "$product_packages_makefile"
@@ -23,9 +24,11 @@ function generate_icon_shape() {
     sed -i "s|?icon_mask|$shape|g" "$config_file"
     sed -i "s|?dialogCornerRadius|$dialogCornerRadius|g" "$config_file"
     sed -i "s|?bottomDialogCornerRadius|$bottomDialogCornerRadius|g" "$config_file"
+    sed -i "s|?useRoundIcon|$useRoundIcon|g" "$config_file"
 }
 
 # Clean previous makefile
 rm -f "$product_packages_makefile"
 
-generate_icon_shape "Hexagon" "M 50,0 L 6.699,25 L 6.699,75 L 50,100 L 93.301,75 L 93.301,25 Z" "0dp" "0dp"
+generate_icon_shape "Hexagon" "M 50,0 L 6.699,25 L 6.699,75 L 50,100 L 93.301,75 L 93.301,25 Z" "0dp" "0dp" "false"
+generate_icon_shape "Round" "M50 0A50 50,0,1,1,50 100A50 50,0,1,1,50 0" "4dp" "8dp" "true"

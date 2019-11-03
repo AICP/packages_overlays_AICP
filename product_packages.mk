286bc7194570c1058d34e156d1b81d155082194c
SpiritCroc <dev@spiritcroc.de>
2019-11-03 18:58:45 +0100

    Bring back QS icon plain theme
    Change-Id: I892872de9e9493337a7cf4248f4313dec5d6b91c

--- product_packages.mk
+++ product_packages.mk
@@ -2,3 +2,4 @@ include packages/overlays/AICP/product_packages_accent.mk
 include packages/overlays/AICP/product_packages_icon_shapes.mk
 include packages/overlays/AICP/product_packages_background_themes.mk
 include packages/overlays/AICP/product_packages_dark.mk
+include packages/overlays/AICP/product_packages_qs_styles.mk

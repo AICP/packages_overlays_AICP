ddde3a237068e293a75d9f84eba38f876baacaae
SpiritCroc <dev@spiritcroc.de>
2019-11-04 15:17:02 +0100

    Add some more QS + notification theming [1/2]
    Change-Id: I107a01b35f96be71b25e0ce037cf240ea2263e94

---
+++ generate_notification_styles.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+my_path="$(dirname "$(realpath "$0")")"
+
+# Get overlay generation functions
+. "$my_path"/generate_overlay.sh
+
+notif_dark_overlay_package="com.aicp.overlay.notif_dark"
+notif_light_overlay_package="com.aicp.overlay.notif_light"
+product_packages_makefile="$my_path/product_packages_notif_styles.mk"
+
+function generate_notif_dark_style() {
+    # generate_notif_dark_style <name> <background>
+    name="$1"
+    background="$2"
+    background_dimmed="$3"
+    shade="$4"
+    name_lc=`echo "$name" | tr '[:upper:]' '[:lower:]'`
+
+    out_dir="$my_path/NotifDark-SystemUI-$name"
+    generate_overlay --night "theming_notif_dark_$name_lc" "aicp.notif_dark" "$my_path/notif_template" "$out_dir" "com.android.systemui" "$notif_dark_overlay_package.$name_lc" "SystemUI" "$product_packages_makefile"
+    colors_file="$out_dir/res/values-night/colors.xml"
+    sed -i "s|?background_dimmed|$background_dimmed|g" "$colors_file"
+    sed -i "s|?shade|$shade|g" "$colors_file"
+    sed -i "s|?background|$background|g" "$colors_file"
+
+    out_dir="$my_path/NotifDark-System-$name"
+    generate_overlay --night "theming_notif_dark_$name_lc" "aicp.notif_dark" "$my_path/notif_template" "$out_dir" "android" "$notif_dark_overlay_package.$name_lc" "" "$product_packages_makefile"
+    colors_file="$out_dir/res/values-night/colors.xml"
+    sed -i "s|?background|$background|g" "$colors_file"
+}
+
+function generate_notif_light_style() {
+    # generate_notif_light_style <name> <background> <background_dimmed> <shade>
+    name="$1"
+    background="$2"
+    background_dimmed="$3"
+    shade="$4"
+    name_lc=`echo "$name" | tr '[:upper:]' '[:lower:]'`
+
+    out_dir="$my_path/NotifLight-SystemUI-$name"
+    generate_overlay "theming_notif_light_$name_lc" "aicp.notif_light" "$my_path/notif_template" "$out_dir" "com.android.systemui" "$notif_light_overlay_package.$name_lc" "SystemUI" "$product_packages_makefile"
+    colors_file="$out_dir/res/values/colors.xml"
+    sed -i "s|?background_dimmed|$background_dimmed|g" "$colors_file"
+    sed -i "s|?shade|$shade|g" "$colors_file"
+    sed -i "s|?background|$background|g" "$colors_file"
+
+    out_dir="$my_path/NotifLight-System-$name"
+    generate_overlay "theming_notif_light_$name_lc" "aicp.notif_light" "$my_path/notif_template" "$out_dir" "android" "$notif_light_overlay_package.$name_lc" "" "$product_packages_makefile"
+    colors_file="$out_dir/res/values/colors.xml"
+    sed -i "s|?background|$background|g" "$colors_file"
+}
+
+# Clean previous makefile
+rm -f "$product_packages_makefile"
+
+generate_notif_dark_style "Gray" "#ff303030" "#aa000000" "#212121"
+# For transparent styles, we can't use shade = 0 or invalidate won't be called in NotificationStackScrollLayout's updateBackgroundDimming as mCachedBackgroundColor == color
+# #00000001 looks the same though.
+generate_notif_dark_style "TransparentGray50" "#80303030" "#80000000" "#00000001"
+generate_notif_dark_style "TransparentGray75" "#b3303030" "#80000000" "#00000001"
+generate_notif_dark_style "TransparentBlack50" "#80000000" "#60000000" "#00000001"
+generate_notif_dark_style "TransparentBlack75" "#b3000000" "#80000000" "#00000001"
+generate_notif_light_style "TransparentWhite" "#b3ffffff" "#80ffffff" "#00000001"
--- generate_qs_styles.sh
+++ generate_qs_styles.sh
@@ -5,8 +5,9 @@ my_path="$(dirname "$(realpath "$0")")"
 # Get overlay generation functions
 . "$my_path"/generate_overlay.sh
 
-plain_template="$my_path/plain_qs_template"
 fg_overlay_package="com.aicp.overlay.qs_fg"
+bg_dark_overlay_package="com.aicp.overlay.qs_bg_dark"
+bg_light_overlay_package="com.aicp.overlay.qs_bg_light"
 product_packages_makefile="$my_path/product_packages_qs_styles.mk"
 
 function generate_qs_fg_style() {
@@ -18,7 +19,39 @@ function generate_qs_fg_style() {
     generate_overlay "theming_qs_fg_$name_lc" "aicp.qs_fg" "$template" "$out_dir" "com.android.systemui" "$fg_overlay_package.$name_lc" "" "$product_packages_makefile"
 }
 
+function generate_qs_bg_dark_style() {
+    # generate_qs_bg_dark_style <name> <background_floating> <panel_background>
+    name="$1"
+    background_floating="$2"
+    panel_background="$3"
+    out_dir="$my_path/QsBgDark-$name"
+    name_lc=`echo "$name" | tr '[:upper:]' '[:lower:]'`
+    generate_overlay --night "theming_qs_bg_dark_$name_lc" "aicp.qs_bg_dark" "$my_path/qs_bg_template" "$out_dir" "com.android.systemui" "$bg_dark_overlay_package.$name_lc" "SystemUI" "$product_packages_makefile"
+    style_file="$out_dir/res/values-night/styles.xml"
+    sed -i "s|?background_floating|$background_floating|g" "$style_file"
+    sed -i "s|?panel_background|$panel_background|g" "$style_file"
+}
+
+function generate_qs_bg_light_style() {
+    # generate_qs_bg_light_style <name> <background_floating> <panel_background>
+    name="$1"
+    background_floating="$2"
+    panel_background="$3"
+    out_dir="$my_path/QsBgLight-$name"
+    name_lc=`echo "$name" | tr '[:upper:]' '[:lower:]'`
+    generate_overlay "theming_qs_bg_light_$name_lc" "aicp.qs_bg_light" "$my_path/qs_bg_template" "$out_dir" "com.android.systemui" "$bg_light_overlay_package.$name_lc" "SystemUI" "$product_packages_makefile"
+    style_file="$out_dir/res/values/styles.xml"
+    sed -i "s|?background_floating|$background_floating|g" "$style_file"
+    sed -i "s|?panel_background|$panel_background|g" "$style_file"
+}
+
 # Clean previous makefile
 rm -f "$product_packages_makefile"
 
 generate_qs_fg_style "Plain" "$my_path/qs_fg_plain_template"
+generate_qs_bg_dark_style "Gray" "#ff303030" "#ff424242"
+generate_qs_bg_dark_style "TransparentBlack50" "#80000000" "#80303030"
+generate_qs_bg_dark_style "TransparentBlack75" "#b3000000" "#b3303030"
+generate_qs_bg_dark_style "TransparentGray50" "#80303030" "#80424242"
+generate_qs_bg_dark_style "TransparentGray75" "#b3303030" "#b3424242"
+generate_qs_bg_light_style "TransparentWhite" "#b3ffffff" "#b3eeeeee"

89aa87021409a36c65a5cc96efa8b6e37d0263d1
SpiritCroc <dev@spiritcroc.de>
2019-11-28 20:34:32 +0100

    Automatic theme update
    Change-Id: Iae25e332c456867076713559cc903a78b6e4156e

---
+++ NotifLight-SystemUI-TransparentWhite/Android.mk
@@ -0,0 +1,13 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := NotifLight-SystemUI-TransparentWhite
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := NotifLight-SystemUI-TransparentWhite
+LOCAL_RESOURCE_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/APPS/SystemUI_intermediates/package.apk
+LOCAL_AAPT_FLAGS := -I $(TARGET_OUT_INTERMEDIATES)/APPS/SystemUI_intermediates/package.apk
+
+include $(BUILD_RRO_PACKAGE)

2a23ebb6b7abb60f995a28cfe6ff5bd820a77954
SpiritCroc <dev@spiritcroc.de>
2019-11-28 11:55:55 +0100

    Automatic theme update
    Change-Id: Ib86e7fd5f9962694911296cd4f4712d9c6370127

---
+++ product_packages_notif_styles.mk
@@ -0,0 +1,9 @@
+PRODUCT_PACKAGES += \
+    NotifDark-SystemUI-Gray \
+    NotifDark-System-NotifDark-SystemUI-Gray \
+    NotifDark-SystemUI-TransparentGray \
+    NotifDark-System-NotifDark-SystemUI-TransparentGray \
+    NotifDark-SystemUI-TransparentBlack \
+    NotifDark-System-NotifDark-SystemUI-TransparentBlack \
+    NotifLight-SystemUI-TransparentWhite \
+    NotifLight-System-NotifLight-SystemUI-TransparentWhite \

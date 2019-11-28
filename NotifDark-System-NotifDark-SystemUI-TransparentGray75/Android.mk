e8d3eccdb7c6e56d310f9dee0bfdfc911ca7846b
SpiritCroc <dev@spiritcroc.de>
2019-11-28 20:44:48 +0100

    Automatic theme update
    Change-Id: Ib86e7fd5f9962694911296cd4f4712d9c6370127

---
+++ NotifDark-System-NotifDark-SystemUI-TransparentGray75/Android.mk
@@ -0,0 +1,11 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := NotifDark-System-NotifDark-SystemUI-TransparentGray75
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := NotifDark-System-NotifDark-SystemUI-TransparentGray75
+
+include $(BUILD_RRO_PACKAGE)
--- product_packages_qs_styles.mk
+++ product_packages_qs_styles.mk
@@ -1,2 +1,8 @@
 PRODUCT_PACKAGES += \
     QsIconsFg-Plain \
+    QsBgDark-Gray \
+    QsBgDark-TransparentBlack50 \
+    QsBgDark-TransparentBlack75 \
+    QsBgDark-TransparentGray50 \
+    QsBgDark-TransparentGray75 \
+    QsBgLight-TransparentWhite \

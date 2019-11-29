d863c289a941af28d2b0638934ef1f9e7b834bc8
SpiritCroc <dev@spiritcroc.de>
2019-11-29 17:42:10 +0100

    Notification themes: hey, there was a TODO for notification guts!
    Change-Id: Ia684aac3c93b57e2a22ac1331ccecda210994d58

--- generate_notification_styles.sh
+++ generate_notification_styles.sh
@@ -18,14 +18,14 @@ function generate_notif_dark_style() {
     name_lc=`echo "$name" | tr '[:upper:]' '[:lower:]'`
 
     out_dir="$my_path/NotifDark-SystemUI-$name"
-    generate_overlay --night "theming_notif_dark_$name_lc" "aicp.notif_dark" "$my_path/notif_template" "$out_dir" "com.android.systemui" "$notif_dark_overlay_package.$name_lc" "SystemUI" "$product_packages_makefile"
+    generate_overlay --night "theming_notif_dark_$name_lc" "aicp.notif_dark" "$my_path/notif_template_dark" "$out_dir" "com.android.systemui" "$notif_dark_overlay_package.$name_lc" "SystemUI" "$product_packages_makefile"
     colors_file="$out_dir/res/values-night/colors.xml"
     sed -i "s|?background_dimmed|$background_dimmed|g" "$colors_file"
     sed -i "s|?shade|$shade|g" "$colors_file"
     sed -i "s|?background|$background|g" "$colors_file"
 
     out_dir="$my_path/NotifDark-System-$name"
-    generate_overlay --night "theming_notif_dark_$name_lc" "aicp.notif_dark" "$my_path/notif_template" "$out_dir" "android" "$notif_dark_overlay_package.$name_lc" "" "$product_packages_makefile"
+    generate_overlay --night "theming_notif_dark_$name_lc" "aicp.notif_dark" "$my_path/notif_template_dark" "$out_dir" "android" "$notif_dark_overlay_package.$name_lc" "" "$product_packages_makefile"
     colors_file="$out_dir/res/values-night/colors.xml"
     sed -i "s|?background|$background|g" "$colors_file"
 }
@@ -39,14 +39,14 @@ function generate_notif_light_style() {
     name_lc=`echo "$name" | tr '[:upper:]' '[:lower:]'`
 
     out_dir="$my_path/NotifLight-SystemUI-$name"
-    generate_overlay "theming_notif_light_$name_lc" "aicp.notif_light" "$my_path/notif_template" "$out_dir" "com.android.systemui" "$notif_light_overlay_package.$name_lc" "SystemUI" "$product_packages_makefile"
+    generate_overlay "theming_notif_light_$name_lc" "aicp.notif_light" "$my_path/notif_template_light" "$out_dir" "com.android.systemui" "$notif_light_overlay_package.$name_lc" "SystemUI" "$product_packages_makefile"
     colors_file="$out_dir/res/values/colors.xml"
     sed -i "s|?background_dimmed|$background_dimmed|g" "$colors_file"
     sed -i "s|?shade|$shade|g" "$colors_file"
     sed -i "s|?background|$background|g" "$colors_file"
 
     out_dir="$my_path/NotifLight-System-$name"
-    generate_overlay "theming_notif_light_$name_lc" "aicp.notif_light" "$my_path/notif_template" "$out_dir" "android" "$notif_light_overlay_package.$name_lc" "" "$product_packages_makefile"
+    generate_overlay "theming_notif_light_$name_lc" "aicp.notif_light" "$my_path/notif_template_light" "$out_dir" "android" "$notif_light_overlay_package.$name_lc" "" "$product_packages_makefile"
     colors_file="$out_dir/res/values/colors.xml"
     sed -i "s|?background|$background|g" "$colors_file"
 }

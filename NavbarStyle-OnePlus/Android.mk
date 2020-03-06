60c7717f5ef199a309239ace0f5a283a767acde3
Alex <du.alexcruz@gmail.com>
2020-03-06 20:48:20 +0300

    Navbar: Introduce navbar styles [1/2]
    Change-Id: I5221e26bdd13d8e8c072d42bae0bd4b6833a05af

---
+++ NavbarStyle-OnePlus/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_RRO_THEME := NavbarStyle-OnePlus
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_SDK_VERSION := current
+LOCAL_PRIVILEGED_MODULE := false
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := NavbarStyle-OnePlus
+
+include $(BUILD_RRO_PACKAGE)
---
+++ product_packages_navbar_styles.mk
@@ -0,0 +1,5 @@
+PRODUCT_PACKAGES += \
+    NavbarStyle-Asus \
+    NavbarStyle-OnePlus \
+    NavbarStyle-OneUi \
+    NavbarStyle-TecnoCamon

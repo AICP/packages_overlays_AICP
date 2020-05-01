bca401964facbf1349d45c7836b7d5ef0e550712
SpiritCroc <dev@spiritcroc.de>
2020-05-01 12:09:42 +0200

    Automatic theme update
    Change-Id: I0cefc45c8b5b5d0af1c10b3e5bd42cbedb7230dd

---
+++ product_packages_corner_radius.mk
@@ -0,0 +1,5 @@
+PRODUCT_PACKAGES += \
+    CornerRadius-Square \
+    CornerRadius-Round_2_2 \
+    CornerRadius-Round_4_8 \
+    CornerRadius-Round_8_16 \
--- product_packages_icon_shapes.mk
+++ product_packages_icon_shapes.mk
@@ -1,5 +1,8 @@
 PRODUCT_PACKAGES += \
+    IconShape-Round \
+    IconShape-Squircle \
     IconShape-SlightlyRounded \
+    IconShape-RoundedRect \
+    IconShape-Teardrop \
     IconShape-Square \
     IconShape-Hexagon \
-    IconShape-Round \

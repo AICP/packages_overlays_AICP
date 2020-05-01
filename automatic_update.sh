7d78806a5158f18c6dd17cc086ecd63bdae64c97
SpiritCroc <dev@spiritcroc.de>
2020-05-01 11:49:41 +0200

    Separate icon shape from round corner setting
    Change-Id: I6f9c0c0a4aad8022eef9f6c6367a81685c3dad37

--- automatic_update.sh
+++ automatic_update.sh
@@ -5,6 +5,7 @@ git reset HEAD --hard
 # Build updates
 ./generate_accents.sh
 ./generate_background_themes.sh
+./generate_corner_radius.sh
 ./generate_default_dark.sh
 ./generate_icon_shapes.sh
 ./generate_notification_styles.sh
--- product_packages.mk
+++ product_packages.mk
@@ -1,4 +1,5 @@
 include packages/overlays/AICP/product_packages_accent.mk
+include packages/overlays/AICP/product_packages_corner_radius.mk
 include packages/overlays/AICP/product_packages_icon_shapes.mk
 include packages/overlays/AICP/product_packages_background_themes.mk
 include packages/overlays/AICP/product_packages_dark.mk

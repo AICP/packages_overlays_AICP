4d827ae82e6457d58b8bab48af042d23100f47dc
doc HD <doc.divxm@gmail.com>
2020-10-20 18:23:25 +0300

    overlays_AICP: exclude dark & notif_styles & background_themes for now
    ** Dialer is broken with dark
    
    Change-Id: I89af7939ec7ece6ecc912ad0d7e6c0ff88b39d6e

--- product_packages.mk
+++ product_packages.mk
@@ -1,7 +1,7 @@
 include packages/overlays/AICP/product_packages_accent.mk
 include packages/overlays/AICP/product_packages_corner_radius.mk
 include packages/overlays/AICP/product_packages_icon_shapes.mk
-include packages/overlays/AICP/product_packages_background_themes.mk
-include packages/overlays/AICP/product_packages_dark.mk
-include packages/overlays/AICP/product_packages_notif_styles.mk
+#include packages/overlays/AICP/product_packages_background_themes.mk
+#include packages/overlays/AICP/product_packages_dark.mk
+#include packages/overlays/AICP/product_packages_notif_styles.mk
 include packages/overlays/AICP/product_packages_qs_styles.mk

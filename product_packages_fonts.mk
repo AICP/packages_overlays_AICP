d40d9a59e6316ba8a4ceeaec23b7e540251d424f
Ali B <abittin@gmail.com>
2021-09-04 21:23:32 +0300

    AicpOverlays: move Fonts to new home
    Change-Id: Iad892d9c5605b871d7bc65fb171edc965b5dd27e

---
+++ product_packages_fonts.mk
@@ -0,0 +1,10 @@
+PRODUCT_PACKAGES += \
+    FontUbuntuOverlay \
+    FontOpenSansOverlay \
+    FontComfortaaOverlay \
+    FontCaviarDreamsOverlay \
+    FontExotwoOverlay \
+    FontSnigletOverlay \
+    FontSignikaOverlay \
+    FontOxaniumOverlay \
+    FontInterOverlay

a3bc5e59c6ce6c740de10f5d4862189fb4668a26
Semavi Ulusoy <doc.divxm@gmail.com>
2021-09-08 15:34:46 +0300

    overlays: VolumePlugins: Adopt to AICP structure
    Change-Id: I0e472bf5ef2273bcdbb7397292a1d8a82988100d

--- ShapeShiftPanel/src/com/android/plugin/volume/ssos/VolumeDialogImpl.java
+++ ShapeShiftPanel/src/com/android/plugin/volume/ssos/VolumeDialogImpl.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package co.potatoproject.plugin.volume.ssos;
+package com.android.plugin.volume.ssos;
 
 import static android.app.ActivityManager.LOCK_TASK_MODE_NONE;
 import static android.media.AudioManager.RINGER_MODE_NORMAL;
@@ -32,7 +32,7 @@ import static android.view.View.INVISIBLE;
 import static android.view.View.VISIBLE;
 import static android.view.ViewGroup.LayoutParams.WRAP_CONTENT;
 
-import static co.potatoproject.plugin.volume.common.Events.DISMISS_REASON_SETTINGS_CLICKED;
+import static com.android.plugin.volume.common.Events.DISMISS_REASON_SETTINGS_CLICKED;
 
 import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
@@ -105,9 +105,9 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import co.potatoproject.plugin.volume.common.*;
+import com.android.plugin.volume.common.*;
 
-import co.potatoproject.plugin.volume.ssos.R;
+import com.android.plugin.volume.ssos.R;
 
 import com.android.systemui.plugins.ActivityStarter;
 import com.android.systemui.plugins.PluginDependency;

a7bbd9a32ad8f7e32ae0afd7a05eaa7b4e44df65
George Zacharia <george.zcharia@gmail.com>
2022-03-26 16:25:28 +0530

    Enable overlays for Android 12L
    Only fonts, adaptive icon shapes and corner radius overlays for now
    
    Change-Id: I171cbad119cfeffd1dfe97af04875210ebcd3ea4

--- product_packages.mk
+++ product_packages.mk
@@ -1,11 +1,11 @@
-include packages/overlays/AICP/product_packages_accent.mk
+# include packages/overlays/AICP/product_packages_accent.mk
 include packages/overlays/AICP/product_packages_corner_radius.mk
 include packages/overlays/AICP/product_packages_icon_shapes.mk
-include packages/overlays/AICP/product_packages_background_themes.mk
+# include packages/overlays/AICP/product_packages_background_themes.mk
 include packages/overlays/AICP/product_packages_navbar_styles.mk
-include packages/overlays/AICP/product_packages_notif_styles.mk
-include packages/overlays/AICP/product_packages_qs_styles.mk
-include packages/overlays/AICP/product_packages_slider_styles.mk
-include packages/overlays/AICP/product_packages_nav_overlays.mk
+# include packages/overlays/AICP/product_packages_notif_styles.mk
+# include packages/overlays/AICP/product_packages_qs_styles.mk
+# include packages/overlays/AICP/product_packages_slider_styles.mk
+# include packages/overlays/AICP/product_packages_nav_overlays.mk
 include packages/overlays/AICP/product_packages_fonts.mk
-include packages/overlays/AICP/product_packages_volume_panels.mk
+# include packages/overlays/AICP/product_packages_volume_panels.mk

6fa4aff231640ef07794ae6b6ffdc2f5a6dc6154
Julian Veit <claymore1298@gmail.com>
2026-03-07 21:20:07 +0000

    fixup! overlays: Bring back UDFPS animations
    Change-Id: Ib89aa3010c25a414b7212a94b2eddcd33693b307

--- product_packages.mk
+++ product_packages.mk
@@ -15,6 +15,8 @@ include packages/overlays/AICP/product_packages_monet.mk
 # include packages/overlays/AICP/product_packages_nav_overlays.mk
 include packages/overlays/AICP/product_packages_fonts.mk
 # include packages/overlays/AICP/product_packages_volume_panels.mk
+# UDFPS animations
+include packages/overlays/AICP/product_packages_UdfpsAnimations.mk
 
 # Lawnicons
 $(call inherit-product-if-exists, vendor/lawnicons/overlay.mk)
